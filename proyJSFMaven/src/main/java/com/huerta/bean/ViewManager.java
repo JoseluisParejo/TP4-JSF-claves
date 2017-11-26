aa7012df34b8274b523b4486e1a08a7b7354a4a6
JLParejo <joseluisparejofernandez@gmail.com>
2017-11-26 18:13:34 +0100

    Update ViewManager.java
    Quitamos el uso de redis

--- proyJSFMaven/src/main/java/com/huerta/bean/ViewManager.java
+++ proyJSFMaven/src/main/java/com/huerta/bean/ViewManager.java
@@ -5,11 +5,9 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import redis.clients.jedis.Jedis;
 
 @ManagedBean(name="obj")
-@ViewScoped
+
 public class ViewManager implements Serializable{
   private String key;
   private String value;
@@ -22,19 +20,9 @@ public class ViewManager implements Serializable{
   public String getLast() {
     return last;
   }
-  public List<Elemento> getListClaves() {
-    listClaves = new ArrayList<Elemento>();
-    Jedis jedis = new Jedis();
-    Map<String,String> hash_tarea2 = jedis.hgetAll("nombres1H");
-    for(Map.Entry<String,String> entry : hash_tarea2.entrySet()) {
-      listClaves.add(new Elemento(entry.getKey(),entry.getValue()));
-    }
-    return listClaves;
-  }
   public String getKey() {
     return key;
   }
-
   public void setKey(String key) {
     this.key = key;
   }
@@ -44,29 +32,20 @@ public class ViewManager implements Serializable{
   public String getValue() {
     return value;
   }
-
   public void setLast(String last) {
     this.last = last;
   }
-
   public void setValue(String value) {
     this.value = value;
   }
-  
   public String add() {
     System.out.println("=" + this.key + "=" + this.value + "=");
-    Jedis conn = new Jedis();
-    if(!(conn.sismember("nombres1",this.key))) {
-      this.last=this.key;
-      this.good=true;
-      conn.sadd("nombres1",this.key);
-      conn.hset("nombres1H",this.key,this.value);
-    }
-    else {
-      this.good=false;
-    }
+    this.last=this.key;
     this.setKey("");
     this.setValue("");
     return null;
+  }
+  public String mostrar(){
+    return "Key: "+key+"\nValue: "+value+"\nLast: "+last;
   }
 }
